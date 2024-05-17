cd05113e76eb28cf5af6f1ad1fe2333cfaf039f4
YunChan-Oh <oyunchan9@gmail.com>
2024-05-17 12:00:00 +0900

    init

---
+++ src/main/java/bsm/bto/BtoApplication.java
@@ -0,0 +1,13 @@
+package bsm.bto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bsm/bto/BtoApplicationTests.java
@@ -0,0 +1,13 @@
+package bsm.bto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
