0752d1efc427e3e3c5d634f439b2ba2611b0216d
noahmik <s46422574@gmail.com>
2024-05-24 18:30:55 +0900

    feat(oauth) : AuthService 구현

--- src/main/java/bsm/bto/oauth/config/SecurityConfig.java
+++ src/main/java/bsm/bto/oauth/config/SecurityConfig.java
@@ -1,15 +1,17 @@
 package bsm.bto.oauth.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity //Spring Security를 활성화
 public class SecurityConfig {
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception { //
         http
@@ -19,5 +21,9 @@ public class SecurityConfig {
                 )
                 .oauth2Login(withDefaults()); // OAuth2 로그인을 활성화
         return http.build(); //SecurityFilterChain 객체를 빌드하여 반환합니다.
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
