712d312f7f291880cee0ceae323916a68fff03c1
YunChan-Oh <oyunchan9@gmail.com>
2024-06-01 23:24:50 +0900

    fix(user) : User 커스텀 리포지토리 수정

--- src/main/java/bsm/bto/user/domain/repository/UserRepositoryCustom.java
+++ src/main/java/bsm/bto/user/domain/repository/UserRepositoryCustom.java
@@ -1,5 +1,5 @@
-//package bsm.bto.user.domain.repository;
-//
-//public interface UserRepositoryCustom {
-//    int findUserRanking(Long userId);
-//}
+package bsm.bto.user.domain.repository;
+
+public interface UserRepositoryCustom {
+    int findUserRanking(Long userId);
+}
