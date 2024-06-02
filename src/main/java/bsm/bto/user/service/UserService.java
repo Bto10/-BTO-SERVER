d66aa07d5a7571e71eb89c7940c576a40c5b00c7
YunChan-Oh <oyunchan9@gmail.com>
2024-06-02 22:49:44 +0900

    fix(user) : User 서비스 인터페이스 수정

--- src/main/java/bsm/bto/user/service/UserService.java
+++ src/main/java/bsm/bto/user/service/UserService.java
@@ -1,10 +1,8 @@
 package bsm.bto.user.service;
 
-import bsm.bto.user.domain.User;
-
-import java.util.List;
+import bsm.bto.user.presentation.dto.UserInfoResponseDTO;
 
 public interface UserService {
-    User getUserById(Long userId); // 사용자 ID로 사용자 정보 조회
-    List<User> getUsersByMoneyRanking(); // 사용자의 돈에 따른 랭킹 조회
+    UserInfoResponseDTO getUserInfo(Long userId);
+    int getUserRanking(Long userId);
 }
