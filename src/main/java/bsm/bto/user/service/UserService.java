6fbeb9eba9d7920fbfc56bd98b78b0550c199813
YunChan-Oh <oyunchan9@gmail.com>
2024-06-03 22:56:46 +0900

    add(user) : UserService 전체 랭킹 구현

--- src/main/java/bsm/bto/user/service/UserService.java
+++ src/main/java/bsm/bto/user/service/UserService.java
@@ -1,8 +1,11 @@
 package bsm.bto.user.service;
 
-import bsm.bto.user.presentation.dto.UserInfoResponseDTO;
+import bsm.bto.user.presentation.dto.*;
+
+import java.util.List;
 
 public interface UserService {
     UserInfoResponseDTO getUserInfo(Long userId);
     int getUserRanking(Long userId);
+    List<UserInfoResponseDTO> getAllUserRankings();
 }
