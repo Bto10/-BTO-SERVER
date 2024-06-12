7868ddfd722e29e837776214ca46b84edf7e8ef6
YunChan-Oh <oyunchan9@gmail.com>
2024-06-13 02:19:19 +0900

    fix(userbet) : UserBetRepository 수정 - findAllByUserId 추가

--- src/main/java/bsm/bto/userbet/domain/repository/UserBetRepository.java
+++ src/main/java/bsm/bto/userbet/domain/repository/UserBetRepository.java
@@ -3,5 +3,8 @@ package bsm.bto.userbet.domain.repository;
 import bsm.bto.userbet.domain.UserBet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserBetRepository extends JpaRepository<UserBet, Long> {
+    List<UserBet> findAllByUserId(Long userId);
 }
